c4bebc8ec68a362b545c99e0473df5c4790d0318
apurvapatwardhan <avp2798@gmail.com>
2025-02-18 12:24:01 +0530

    add header, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Body from './components/Body'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
-  return<>
-  <Navbar />
-  </>
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<div>Feed</div>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/connections" element={<div>connections</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
